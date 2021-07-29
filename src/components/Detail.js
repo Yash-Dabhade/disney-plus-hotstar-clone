3bdbe4cf763b7da1d775643216fbe14fbb20c3ed
YASH <yashdabhade215@gmail.com>
2021-07-29 21:59:22 +0530

    Done Login UI

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -64,6 +64,7 @@ const ImageTitle = styled.div`
   min-heigth: 170px;
   min-width: 200px;
   width: 35vw;
+  margin-top: 60px;
 
   img {
     width: 100%;
@@ -136,4 +137,5 @@ const Description = styled.div`
   font-size: 20px;
   margin-top: 16px;
   color: rgb(249, 249, 249);
+  max-width: 760px;
 `;
