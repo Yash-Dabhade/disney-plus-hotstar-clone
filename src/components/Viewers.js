2812599df9bd53ab00cafdee128b7e3a92e9e1d0
YASH <yashdabhade215@gmail.com>
2021-07-30 19:28:18 +0530

    completed project

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -6,7 +6,7 @@ function Viewers() {
     <Container>
       <Wrap>
         <img src="/images/viewers-disney.png" alt="Marvel" />
-        <video autoPlay={true} loop={true} playsInline={true}>
+        <video autoPlay={true} loop="true">
           <source src="/videos/disney.mp4" type="video/mp4" />
         </video>
       </Wrap>
@@ -45,21 +45,41 @@ const Container = styled.div`
   display: grid;
   padding: 30px 0px 26px;
   grid-gap: 25px;
+  gap: 25px;
   grid-template-columns: repeat(5, minmax(0, 1fr));
 `;
 
 const Wrap = styled.div`
   border: 3px solid rgba(249, 249, 249, 0.1);
   border-radius: 10px;
+  padding-top: 56.25%;
   cursor: pointer;
+  position: relative;
+  overflow: hidden;
   box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
     rgb(0 0 0 / 73%) 0px 16px 10px -10px;
   transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
 
   img {
+    inset: 0px;
+    display: block;
+    position: absolute;
+    opacity: 1;
     width: 100%;
     height: 100%;
+    top: 0;
+    z-index: 1;
     object-fit: cover;
+    transition: opacity 500ms ease-in-out 0s;
+  }
+
+  video {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0px;
+    opacity: 0;
+    z-index: 0;
   }
 
   &:hover {
@@ -67,12 +87,9 @@ const Wrap = styled.div`
       rgba(0 0 0 / 72%) 0px 30px 22px -10px;
     transform: scale(1.05);
     border-color: rgba(249, 249, 249, 0.8);
-  }
 
-  video {
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    top: 0;
+    video {
+      opacity: 1;
+    }
   }
 `;
